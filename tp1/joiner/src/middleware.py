1e63b19e7d8eb2880463b9a29e3724452ad2da52
Tomas Nocetti <tnocetti@fi.uba.ar>
2022-09-26 22:34:30 -0300

    fix: remove unused queue

--- tp1/joiner/src/middleware.py
+++ tp1/joiner/src/middleware.py
@@ -1,6 +1,5 @@
 from common.middleware import Middleware
 
-RAW_DATA_QUEUE = 'raw_data_queue'
 CATEGORIES_QUEUE = 'categories_queue'
 VIDEO_DATA_QUEUE = 'video_data'
 DISTRIBUTION_EXCHANGE = 'distribution_exchange'
@@ -9,7 +8,6 @@ DISTRIBUTION_EXCHANGE = 'distribution_exchange'
 class JoinerMiddlware(Middleware):
     def __init__(self) -> None:
         super().__init__()
-        self.channel.queue_declare(queue=RAW_DATA_QUEUE)
         self.channel.queue_declare(
             queue=CATEGORIES_QUEUE)
         self.channel.queue_declare(
