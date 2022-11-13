bb0f216279e91ca12717b9a780d22c6cde0937ce
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-11-12 21:02:33 -0300

    Add hearbeats constants

--- tp1/common/constants.py
+++ tp1/common/constants.py
@@ -2,5 +2,5 @@ CATEGORY_SUBFIX = '_category_id.json'
 DATA_SUBFIX = '_youtube_trending_data.csv'
 
 NO_HEARTBEAT_CODE = -1
-HEARTBEAT_FRECUENCY = 1 #In seconds
-SERVICE_HEARTBEAT_TIMEOUT = 5 #In seconds
+HEARTBEAT_FRECUENCY = 5 #In seconds
+SERVICE_HEARTBEAT_TIMEOUT = 10 #In seconds
